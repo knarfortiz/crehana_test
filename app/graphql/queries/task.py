f9d6a21a413c17910604f82e02f0c00d90c76e58
knarfortiz <o.g.knarf@hotmail.com>
2025-08-06 11:04:57 -0500

    create query for get all tasks

--- app/graphql/queries/task.py
+++ app/graphql/queries/task.py
@@ -1,23 +1,31 @@
 from typing import List
 
 import strawberry
+from sqlmodel import Session
+from strawberry.types import Info
 
-from app.graphql.types import TaskType
+from app.graphql.types import TaskPriority, TaskStatus, TaskType
+from app.infrastructure.db.repositories import get_all_tasks
 
 
 @strawberry.type
 class TaskQueries:
     @strawberry.field
-    def tasks(self) -> List[TaskType]:
+    def tasks(self, info: Info) -> List[TaskType]:
+        session: Session = info.context["session"]
+
+        db_tasks = get_all_tasks(session)
+
         return [
             TaskType(
-                id=1,
-                title="Tarea de prueba",
-                description="Descripción demo",
-                is_done=False,
-                status="pending",
-                priority="medium",
-                list_id=1,
-                assigned_to=None,
+                id=task.id,
+                title=task.title,
+                description=task.description,
+                is_done=task.is_done,
+                status=TaskStatus(task.status),
+                priority=TaskPriority(task.priority),
+                list_id=task.list_id,
+                assigned_to=None  # aún no usamos usuarios
             )
+            for task in db_tasks
         ]
